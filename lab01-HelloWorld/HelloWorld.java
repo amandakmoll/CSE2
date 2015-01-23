a2aa08a329fe83a4af492fd83a3a98cec030d3ce
amandakmoll <amandakmoll@gmail.com>
2015-01-23 19:41:09 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+//Amanda Moll
+//Lab 1
+//Hello World Java Program
+
+// Define class
+public class HelloWorld{
+    
+    // main method
+    public static void main(String[] args){
+        
+        //print statement
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test
+    }
+}
