d77c33bdf6c4007015d24eef202f10f388ab0094
amandakmoll <amandakmoll@gmail.com>
2015-01-26 20:40:03 +0000

    update hw01

---
+++ hw01/hw01.java
@@ -0,0 +1,23 @@
+//Amanda Moll
+//Brian Chen
+//Homework 1
+
+//Define Class
+public class hw01{
+    
+    //main method
+    public static void main(String[] args){
+        
+        //print statements
+        System.out.println("   -----------  ");
+        System.out.println("   | WELCOME |  ");
+        System.out.println("   -----------  ");
+        System.out.println(" ^  ^  ^  ^  ^  ^ ");
+        System.out.println("'/''\'/''\'/''\'/''\'/''\'/''\'");
+        System.out.println("<-A--K--M--2--1--5->");
+        System.out.println("'\'/'\'/'\'/'\'/'\'/'\'/'");
+        System.out.println(" v  v  v  v  v  v");
+        //about myself
+        System.out.println("I went snowboarding with my family over winter break. Well, they snowboarded while I tumbled down the hill...");
+    }
+}
